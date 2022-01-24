3044ac06cad491a2cdddea718d434c0589498e89
Pedro Miranda <pbragamiranda@gmail.com>
2022-01-24 12:34:12 +0100

    small fixes + update db

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -32,7 +32,7 @@ const initMapbox = () => {
     const fitMapToMarkers = (map, markers) => {
       const bounds = new mapboxgl.LngLatBounds();
       markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3 });
+      map.fitBounds(bounds, { padding: 200, maxZoom: 100, duration: 3 });
     };
     
     fitMapToMarkers(map, markers);
